11f40a24ba51c354fa21abb4d5a54af952607536
Hadeel-Ayman <haddelayman4@gmail.com>
2023-05-20 18:09:03 +0300

    add changes

--- frontend/src/component/Button/index.js
+++ frontend/src/component/Button/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import './style.scss'
 
-const Button = ({ type, title, src, color, backgroundColor }) => {
+const Button = ({ title, src, color, backgroundColor }) => {
     return (
         <button
             className='mediaBtn'
-            type={type}
             style={{ backgroundColor, color }}>
             <img src={src} alt='' />
             <h4>{title}</h4>
