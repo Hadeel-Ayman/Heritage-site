24bef19a95774e25ae8b3b0777084452f8fea2d5
hebamesluh1 <hebamesluh@gmail.com>
2023-05-21 16:53:06 +0300

    finished ui of seller profile

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,6 @@
 import Login from "./Login";
 import Register from "./Register";
 import Profile from "./Profile";
+import SellerProfile from "./SellerProfile";
 
-export {
-    Login, Register, Profile
-}
+export { Login, Register, Profile, SellerProfile };
