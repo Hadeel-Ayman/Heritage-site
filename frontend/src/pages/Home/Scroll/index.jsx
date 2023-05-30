e04afa8d733adb1e48564a26e06bdcf34e654f47
hebamesluh1 <hebamesluh@gmail.com>
2023-05-30 13:33:23 +0300

    update

--- frontend/src/pages/Home/Scroll/index.jsx
+++ frontend/src/pages/Home/Scroll/index.jsx
@@ -8,71 +8,71 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 
-const Scroll = ({title}) => {
+const Scroll = ({ title }) => {
     return (
         <div className='scroll-container'>
             <h2>{title} </h2>
             <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-         <SwiperSlide>
-         <ServiceComponent
-                src="https://placehold.co/400x400"
-                img="https://placehold.co/400x400"
-                name="john"
-                desc="description"
-                price="300$"
-            />
-         </SwiperSlide>
-         <SwiperSlide>
-         <ServiceComponent
-                src="https://placehold.co/400x400"
-                img="https://placehold.co/400x400"
-                name="john"
-                desc="description"
-                price="300$"
-            />
-         </SwiperSlide>
-         <SwiperSlide>
-         <ServiceComponent
-                src="https://placehold.co/400x400"
-                img="https://placehold.co/400x400"
-                name="john"
-                desc="description"
-                price="300$"
-            />
-         </SwiperSlide>
-         <SwiperSlide>
-         <ServiceComponent
-                src="https://placehold.co/400x400"
-                img="https://placehold.co/400x400"
-                name="john"
-                desc="description"
-                price="300$"
-            />
-         </SwiperSlide>
-         <SwiperSlide>
-         <ServiceComponent
-                src="https://placehold.co/400x400"
-                img="https://placehold.co/400x400"
-                name="john"
-                desc="description"
-                price="300$"
-            />
-         </SwiperSlide>
-         <SwiperSlide>
-         <ServiceComponent
-                src="https://placehold.co/400x400"
-                img="https://placehold.co/400x400"
-                name="john"
-                desc="description"
-                price="300$"
-            />
-         </SwiperSlide>
-    </Swiper>
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <ServiceComponent
+                        src="https://placehold.co/400x400"
+                        img="https://placehold.co/400x400"
+                        name="john"
+                        desc="description"
+                        price="300$"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <ServiceComponent
+                        src="https://placehold.co/400x400"
+                        img="https://placehold.co/400x400"
+                        name="john"
+                        desc="description"
+                        price="300$"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <ServiceComponent
+                        src="https://placehold.co/400x400"
+                        img="https://placehold.co/400x400"
+                        name="john"
+                        desc="description"
+                        price="300$"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <ServiceComponent
+                        src="https://placehold.co/400x400"
+                        img="https://placehold.co/400x400"
+                        name="john"
+                        desc="description"
+                        price="300$"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <ServiceComponent
+                        src="https://placehold.co/400x400"
+                        img="https://placehold.co/400x400"
+                        name="john"
+                        desc="description"
+                        price="300$"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <ServiceComponent
+                        src="https://placehold.co/400x400"
+                        img="https://placehold.co/400x400"
+                        name="john"
+                        desc="description"
+                        price="300$"
+                    />
+                </SwiperSlide>
+            </Swiper>
         </div>
     )
 }
