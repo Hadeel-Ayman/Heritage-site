cf243f2192824fd1f610f480c5fd52b4160d9321
hebamesluh1 <hebamesluh@gmail.com>
2023-06-21 16:14:21 +0300

    integration api's in login and registers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@ import Router from "./router";
 import Header from "./component/Layout/Header";
 import Footer from "./component/Layout/Footer";
 import EditProvider from "./context/editContext";
+import AuthProvider from "./context/authContext";
 
 function App() {
   return (
     <div className="App">
-      <EditProvider>
-        <Header />
-        <Router />
-        <Footer /> 
-      </EditProvider>
+      <AuthProvider>
+        <EditProvider>
+          <Header />
+          <Router />
+          <Footer />
+        </EditProvider>
+      </AuthProvider>
     </div>
   );
 }
